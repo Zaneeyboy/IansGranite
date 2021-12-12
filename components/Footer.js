9722a0949db42e982ba7ecdfde66955e8f3c0796
Zaneeyboy <40801075+Zaneeyboy@users.noreply.github.com>
2021-12-12 11:14:06 -0500

    Styled Footer and Social Icon Components

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,31 @@
+import Links from "./Links";
+import styles from "../styles/components/Footer.module.css";
+import Link from "next/link";
 const Footer = () => {
   return (
-    <footer>
-      <p>©2022 Ian's Granite</p>
+    <footer className={styles.footer}>
+      <div className={styles.navigation}>
+        <p className={styles.header}>Navigation</p>
+        <Link href="/">
+          <a className={styles.navLink}>Home</a>
+        </Link>
+        <Link href="/showroom">
+          <a className={styles.navLink}>Showroom</a>
+        </Link>
+        <Link href="/projects">
+          <a className={styles.navLink}>Projects</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.navLink}>Contact Us</a>
+        </Link>
+      </div>
+      <Links />
+      <div className={styles.links}>
+        <p>©2022 Ian's Granite</p>
+        <p className={styles.address}>
+          #106 Bejucal Road, Cunupia, Trinidad. W.I.
+        </p>
+      </div>
     </footer>
   );
 };
--- components/Links.js
+++ components/Links.js
@@ -1,5 +1,3 @@
-import styles from "../styles/components/Banner.module.css";
-import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
@@ -7,8 +5,9 @@ import {
   faTiktok,
   faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/components/Links.module.css";
 
 const Links = () => {
   return (
@@ -37,31 +36,4 @@ const Links = () => {
     </>
   );
 };
-
-const Banner = () => {
-  return (
-    <section>
-      <div className={styles.banner}>
-        <div className={styles.lead}>
-          <p>Suppliers of granite, marble and quartz countertops.</p>
-        </div>
-        <div className={styles.lead_cta_container}>
-          <a className={styles.lead_cta} href="tel:8686204109">
-            Call for a free consultation!
-          </a>
-        </div>
-        <Links />
-      </div>
-
-      <Image
-        src="/images/brown.jpg"
-        width={2000}
-        height={750}
-        layout="intrinsic"
-        alt="Countertop"
-        priority={true}
-      />
-    </section>
-  );
-};
-export default Banner;
+export default Links;
